f6ca0876d97488eb803208ce546a6ce1522f460c
Volodymyr_Yamnyi <volodymyryamnyi@gmail.com>
2021-10-21 14:21:32 +0300

    implemented Find numbers which are divisible by given number

--- src/main/java/com/org/ita/kata/implementation/vy1989/Eigth.java
+++ src/main/java/com/org/ita/kata/implementation/vy1989/Eigth.java
@@ -1,5 +1,8 @@
 package com.org.ita.kata.implementation.vy1989;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.org.ita.kata.Eight;
 
 public class Eigth implements Eight {
@@ -43,8 +46,30 @@ public class Eigth implements Eight {
         return 0;
     }
 
-    @Override
-    public int[] divisibleBy(int[] numbers, int divider) {
-        return new int[0];
-    }
+    //Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor.
+  	@Override
+      public int[] divisibleBy(int[] numbers, int divider) {
+      	 List<Integer> list = new ArrayList<Integer>();
+  		 int[] dividedArr = null;
+  		 int dividedArrSize = 0;
+  		 try {
+  		     for(int el : numbers) 
+  			 if(el % divider == 0) 
+  				 list.add(el);
+  		     
+  		     dividedArrSize = list.size();
+  			 dividedArr = new int[dividedArrSize];
+  			 
+  			 for(int i = 0; i < dividedArrSize; i++) 
+  				dividedArr[i] = list.get(i);
+  		 }
+  		 catch(NullPointerException e) {
+  			 System.out.println(e);
+  		 }
+  		 catch(ArithmeticException e) {
+  			 System.out.println(e);
+  		 }
+  		 
+  		 return dividedArr;
+      }
 }
