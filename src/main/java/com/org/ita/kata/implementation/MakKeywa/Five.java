39c29ecbc1a49616d30bbf67420a09f5329cbb30
MakKeywa <mak.keywa@gmail.com>
2021-10-21 23:42:17 +0300

    added implementation of Gap in Primes

--- src/main/java/com/org/ita/kata/implementation/MakKeywa/Five.java
+++ src/main/java/com/org/ita/kata/implementation/MakKeywa/Five.java
@@ -10,7 +10,44 @@ public class Five implements com.org.ita.kata.Five {
 
     @Override
     public long[] gap(int g, long m, long n) {
-        return new long[0];
+        long lastPrime = -1;
+        long i = m + 1;
+        for (; i < n; i++) {
+            if (isPrime(i)) {
+                lastPrime = i;
+                break;
+            }
+        }
+        if (lastPrime == -1) {
+            return null;
+        }
+        for (i = lastPrime + 1; i < n; i++) {
+            if (isPrime(i)) {
+                if (i - lastPrime == g) {
+                    return new long[]{lastPrime, i};
+                }
+                lastPrime = i;
+            }
+        }
+        return null;
+    }
+
+    public static boolean isPrime(long n) {
+        if (n < 0 || n == 0 || n == 1) {
+            return false;
+        }
+        if (n == 2) {
+            return true;
+        }
+        if (n % 2 == 0) {
+            return false;
+        }
+        for (long i = 3; i <= Math.sqrt(n); i += 2) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
     @Override
