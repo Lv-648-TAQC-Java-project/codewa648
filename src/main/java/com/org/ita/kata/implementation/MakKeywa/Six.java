b00853e661465c539a0c13a577066a2756cfcddd
MakKeywa <mak.keywa@gmail.com>
2021-10-21 14:35:02 +0300

    added implementation of Easy Balance Checking

--- src/main/java/com/org/ita/kata/implementation/MakKeywa/Six.java
+++ src/main/java/com/org/ita/kata/implementation/MakKeywa/Six.java
@@ -8,7 +8,23 @@ public class Six implements com.org.ita.kata.Six {
 
     @Override
     public String balance(String book) {
-        return null;
+        String res = "";
+        String endLine = "\\r\\n";
+        String cleanBook = book.replaceAll("[^a-zA-Z0-9. \n]", "").replaceAll(" +", " ");
+        String[] separatedBook = cleanBook.split("\n");
+        float allMoney = Float.parseFloat(separatedBook[0]);
+        float moneyLeft = allMoney;
+        res += "Original Balance: " + String.format("%.2f", allMoney) + endLine;
+        float[] expenses = new float[separatedBook.length - 1];
+        for (int i = 1; i < separatedBook.length; i++) {
+            String[] bookEntry = separatedBook[i].split(" ");
+            expenses[i - 1] = Float.parseFloat(bookEntry[2]);
+            moneyLeft -= expenses[i - 1];
+            res += bookEntry[0] + " " + bookEntry[1] + " " + bookEntry[2] + " Balance " + String.format("%.2f", moneyLeft) + endLine;
+        }
+        res += "Total expense  " + String.format("%.2f", (allMoney - moneyLeft)) + endLine;
+        res += "Average expense  " + String.format("%.2f", (allMoney - moneyLeft) / (separatedBook.length - 1));
+        return res;
     }
 
     @Override
