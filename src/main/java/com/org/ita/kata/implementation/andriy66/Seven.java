2282d3e37bbc81682f6c5905f7be59bf4273d628
Liubomyr Halamaha <galamagal86@gmail.com>
2021-10-21 10:15:34 +0300

    add impl andriy66

---
+++ src/main/java/com/org/ita/kata/implementation/andriy66/Seven.java
@@ -0,0 +1,18 @@
+package com.org.ita.kata.implementation.andriy66;
+
+public class Seven implements com.org.ita.kata.Seven {
+    @Override
+    public long newAvg(double[] arr, double navg) {
+        return 0;
+    }
+
+    @Override
+    public String seriesSum(int n) {
+        return null;
+    }
+
+    @Override
+    public int whereIsHe(int p, int bef, int aft) {
+        return 0;
+    }
+}
