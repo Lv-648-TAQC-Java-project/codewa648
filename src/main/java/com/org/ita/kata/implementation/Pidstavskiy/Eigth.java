92fddd9105b2134d921ea8c5881a728122d526e7
Pidstavskiy <vitalii.pidstavskiy@gmail.com>
2021-10-21 14:48:40 +0300

    Implemented Count of positives / sum of negatives task

--- src/main/java/com/org/ita/kata/implementation/Pidstavskiy/Eigth.java
+++ src/main/java/com/org/ita/kata/implementation/Pidstavskiy/Eigth.java
@@ -25,7 +25,23 @@ public class Eigth implements Eight {
 
     @Override
     public int[] countPositivesSumNegatives(int[] input) {
-        return new int[0];
+        int[] array = new int[2];
+        int count = 0;
+        int sum = 0;
+        if (input != null && input.length != 0) {
+            for (int i = 0; i < input.length; i++) {
+                if (input[i] > 0) {
+                    count++;
+                } else {
+                    sum += input[i];
+                }
+            }
+        } else {
+            return new int[0];
+        }
+        array[0] = count;
+        array[1] = sum;
+        return array;
     }
 
     @Override
