4e2dfeb055582d57857d2f92a90e7db09e266d05
Volodymyr_Yamnyi <volodymyryamnyi@gmail.com>
2021-10-21 17:05:44 +0300

    implemented Looking for a benefactor

--- src/main/java/com/org/ita/kata/implementation/vy1989/Seven.java
+++ src/main/java/com/org/ita/kata/implementation/vy1989/Seven.java
@@ -1,11 +1,21 @@
 package com.org.ita.kata.implementation.vy1989;
 
 public class Seven implements com.org.ita.kata.Seven {
+	//The function new_avg(arr, navg) should return the expected donation (rounded up to the next integer) 
+	//that will permit to reach the average navg.
     @Override
     public long newAvg(double[] arr, double navg) {
-        return 0;
+    	double sum = 0;
+		for(double num : arr) {
+			sum+=num;
+		}
+		long res = (long) Math.ceil((((arr.length + 1) * navg ) - sum));
+        if(res <= 0) throw new IllegalArgumentException();
+        return res;
     }
 
+    //-----------------------------------------------------------------------------------------------------------------
+    
     @Override
     public String seriesSum(int n) {
         return null;
