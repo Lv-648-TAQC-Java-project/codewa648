bd749db86872b35477af7ce322426ff4a2d4db50
Pidstavskiy <vitalii.pidstavskiy@gmail.com>
2021-10-21 16:04:57 +0300

    Changed the implementation, remove unnecessary variables

--- src/main/java/com/org/ita/kata/implementation/Pidstavskiy/Eigth.java
+++ src/main/java/com/org/ita/kata/implementation/Pidstavskiy/Eigth.java
@@ -26,21 +26,17 @@ public class Eigth implements Eight {
     @Override
     public int[] countPositivesSumNegatives(int[] input) {
         int[] array = new int[2];
-        int count = 0;
-        int sum = 0;
         if (input != null && input.length != 0) {
             for (int i = 0; i < input.length; i++) {
                 if (input[i] > 0) {
-                    count++;
+                    array[0]++;
                 } else {
-                    sum += input[i];
+                    array[1] += input[i];
                 }
             }
         } else {
             return new int[0];
         }
-        array[0] = count;
-        array[1] = sum;
         return array;
     }
 
