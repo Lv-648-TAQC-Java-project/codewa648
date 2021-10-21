d40b90622042a27dffd5f99aef3588ae80569ff9
MakKeywa <mak.keywa@gmail.com>
2021-10-21 14:20:45 +0300

    added implementation of Looking for a benefactor

--- src/main/java/com/org/ita/kata/implementation/MakKeywa/Seven.java
+++ src/main/java/com/org/ita/kata/implementation/MakKeywa/Seven.java
@@ -3,7 +3,17 @@ package com.org.ita.kata.implementation.MakKeywa;
 public class Seven implements com.org.ita.kata.Seven {
     @Override
     public long newAvg(double[] arr, double navg) {
-        return 0;
+        int n = arr.length;
+        double sum = 0;
+        double exp;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+        }
+        exp = Math.ceil(navg * (n + 1) - sum);
+        if (exp <= 0) {
+            throw new IllegalArgumentException("too low avg");
+        }
+        return (long) exp;
     }
 
     @Override
