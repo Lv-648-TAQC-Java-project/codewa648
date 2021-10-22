8394aa1a5b071a3664958e4d49c73217d39e944d
Pidstavskiy <vitalii.pidstavskiy@gmail.com>
2021-10-22 19:34:47 +0300

    Implemented Sum of the first nth term of Series task

--- src/main/java/com/org/ita/kata/implementation/Pidstavskiy/Seven.java
+++ src/main/java/com/org/ita/kata/implementation/Pidstavskiy/Seven.java
@@ -8,7 +8,15 @@ public class Seven implements com.org.ita.kata.Seven {
 
     @Override
     public String seriesSum(int n) {
-        return null;
+        double sum = 0.00;
+        if (n == 0) {
+            return String.valueOf(sum);
+        } else {
+            for (int i = 0; i < n; i++) {
+                sum += 1.0 / (1 + i * 3);
+            }
+            return String.format("%.2f", sum);
+        }
     }
 
     @Override
