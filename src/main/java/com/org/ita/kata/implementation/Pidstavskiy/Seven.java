620541a4c4ee7eaed29ca01cebe8cf1a946b638c
Pidstavskiy <vitalii.pidstavskiy@gmail.com>
2021-10-22 19:19:08 +0300

    Implemented Looking for a benefactor task

--- src/main/java/com/org/ita/kata/implementation/Pidstavskiy/Seven.java
+++ src/main/java/com/org/ita/kata/implementation/Pidstavskiy/Seven.java
@@ -3,7 +3,17 @@ package com.org.ita.kata.implementation.Pidstavskiy;
 public class Seven implements com.org.ita.kata.Seven {
     @Override
     public long newAvg(double[] arr, double navg) {
-        return 0;
+        double sum = 0;
+        for (double v : arr) {
+            sum += v;
+        }
+        long result = (long) Math.ceil(navg * (arr.length + 1) - sum);
+        if (result <= 0){
+            throw new IllegalArgumentException();
+        }
+        else {
+            return result;
+        }
     }
 
     @Override
