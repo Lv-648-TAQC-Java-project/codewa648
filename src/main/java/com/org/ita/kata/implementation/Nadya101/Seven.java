5b55a9311697b3aecba3f5aa6b7010930212a254
Nadya Bryashchey <Angel.nadii36@gmail.com>
2021-10-23 00:32:50 +0300

    nadya101/#189 Looking for a benefactor

--- src/main/java/com/org/ita/kata/implementation/Nadya101/Seven.java
+++ src/main/java/com/org/ita/kata/implementation/Nadya101/Seven.java
@@ -3,7 +3,15 @@ package com.org.ita.kata.implementation.Nadya101;
 public class Seven implements com.org.ita.kata.Seven {
     @Override
     public long newAvg(double[] arr, double navg) {
-        return 0;
+        double sum = 0;
+        for (double donation : arr) {
+            sum += donation;
+        }
+        long nextDonation = (long) Math.ceil((arr.length + 1) * navg - sum);
+        if (nextDonation < 0) {
+            throw new IllegalArgumentException("-1");
+        }
+        return nextDonation;
     }
 
     @Override
