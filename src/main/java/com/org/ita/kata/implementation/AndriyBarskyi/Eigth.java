59b829ceafe9f61579ff6177cf58c0c8ff74af21
Andriy Barskyi <barskyiandriy@gmail.com>
2021-10-23 14:26:02 +0300

    implement method divisibleBy in Eigth

--- src/main/java/com/org/ita/kata/implementation/AndriyBarskyi/Eigth.java
+++ src/main/java/com/org/ita/kata/implementation/AndriyBarskyi/Eigth.java
@@ -45,6 +45,23 @@ public class Eigth implements Eight {
 
     @Override
     public int[] divisibleBy(int[] numbers, int divider) {
-        return new int[0];
+        if (divider == 0 || numbers.length == 0) {
+            return new int[]{};
+        }
+        int count = 0;
+        for(int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % divider == 0) {
+                count++;
+            }
+        }
+        int divisible[] = new int[count];
+        int j = 0;
+        for(int i: numbers) {
+            if (i % divider == 0) {
+                divisible[j] = i;
+                j++;
+            }
+        }
+        return divisible;
     }
 }
