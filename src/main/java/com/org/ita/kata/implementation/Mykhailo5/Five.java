b3ac7d52d314694213195c6ee6a5d7e27846bf2b
Mykhailo5 <mykhailomykhailyshyn@gmail.com>
2021-10-26 20:09:44 +0300

    implemented Gap in Primes

--- src/main/java/com/org/ita/kata/implementation/Mykhailo5/Five.java
+++ src/main/java/com/org/ita/kata/implementation/Mykhailo5/Five.java
@@ -8,9 +8,26 @@ public class Five implements com.org.ita.kata.Five {
         return 0;
     }
 
+    public static boolean isPrime(long num){
+        for(int i=2; i < num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
     @Override
     public long[] gap(int g, long m, long n) {
-        return new long[0];
+        long prime = 0;
+        for (long i = m; i < n; i++) {
+            if(isPrime(i)){
+                if(i-prime==g) {
+                    return new long[]{prime, i};
+                }
+                prime=i;
+            }
+        }
+        return null;
     }
 
     @Override
