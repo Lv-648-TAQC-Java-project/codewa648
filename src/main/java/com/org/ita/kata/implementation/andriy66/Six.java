0b00a0520301de88c6627eb47652be0abc4f5097
Andriy <krol17aq@gmail.com>
2021-10-26 18:45:59 +0300

    #304

--- src/main/java/com/org/ita/kata/implementation/andriy66/Six.java
+++ src/main/java/com/org/ita/kata/implementation/andriy66/Six.java
@@ -1,8 +1,10 @@
 package com.org.ita.kata.implementation.andriy66;
 import static java.util.stream.Collectors.averagingDouble;
-
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.ArrayList;
+
 public class Six implements com.org.ita.kata.Six {
     @Override
     public long findNb(long m) {
@@ -75,7 +77,55 @@ public class Six implements com.org.ita.kata.Six {
 
     @Override
     public String nbaCup(String resultSheet, String toFind) {
-        return null;
+        List<String> games = Arrays.stream(resultSheet.split(",")).filter(x -> x.contains(toFind + " ")).collect(Collectors.toList());
+        if (toFind.isEmpty()) {
+            return "";
+        }
+        if (games.isEmpty()) {
+            return toFind+":This team didn't play!";
+        }
+        List<String[]> teams = games.stream().map(game -> game.split("\\s\\d+(\\W|$)")).collect(Collectors.toList());
+        List<Integer[]> scores = games.stream()
+                .map(game -> Arrays.stream(game.split(" "))
+                        .filter(x -> x.matches("\\d+"))
+                        .map(Integer::valueOf).toArray(Integer[]::new))
+                .collect(Collectors.toList());
+        int wins = 0, draws = 0, lose = 0, scored = 0, conceded = 0, points = 0;
+        for (int i = 0; i < teams.size(); i++) {
+            try {
+                boolean draw = scores.get(i)[0] == scores.get(i)[1];
+            } catch (ArrayIndexOutOfBoundsException e) {
+                return "Error(float number):" + games.get(i);
+            }
+            boolean teamFirst = teams.get(i)[0].equals(toFind);
+            if (teamFirst) {
+                scored += scores.get(i)[0];
+                conceded += scores.get(i)[1];
+            } else {
+                scored += scores.get(i)[1];
+                conceded += scores.get(i)[0];
+            }
+            if (scores.get(i)[0] == scores.get(i)[1]) {
+                draws++;
+                points++;
+            } else if (scores.get(i)[0] < scores.get(i)[1]) {
+                if (teamFirst) {
+                    lose++;
+                } else {
+                    wins++;
+                    points += 3;
+                }
+            } else {
+                if (teamFirst) {
+                    wins++;
+                    points += 3;
+                } else {
+                    lose++;
+                }
+            }
+        }
+        return String.format(toFind + ":W=%s;D=%s;L=%s;Scored=%s;Conceded=%s;Points=%s",
+                wins, draws, lose, scored, conceded, points);
     }
 
     @Override
