1379f54c0279bb2cacaa4bbdcd908631355d65ac
Mykhailo5 <mykhailomykhailyshyn@gmail.com>
2021-10-26 19:59:41 +0300

    implemented Rainfall

--- src/main/java/com/org/ita/kata/implementation/Mykhailo5/Six.java
+++ src/main/java/com/org/ita/kata/implementation/Mykhailo5/Six.java
@@ -18,12 +18,54 @@ public class Six implements com.org.ita.kata.Six {
 
     @Override
     public double mean(String town, String strng) {
-        return 0;
+        if (town == null || town.length() == 0 || strng == null || strng.length() == 0) {
+            return -1;
+        }
+
+        final String[] a = strng.split("\n");
+        String[] recs = null;
+        double sum = 0;
+
+        for (String str : a) {
+            if (str.startsWith(town + ":")) {
+                recs = str.split(":")[1].split(",");
+                break;
+            }
+        }
+        if (recs == null || recs.length == 0) {
+            return -1;
+        }
+
+        for (int i = 0; i < recs.length; i++) {
+            sum += Double.parseDouble(recs[i].split(" ")[1]);
+        }
+        return sum / 12.0;
     }
 
     @Override
     public double variance(String town, String strng) {
-        return 0;
+        if (town == null || town.length() == 0 || strng == null || strng.length() == 0) {
+            return -1;
+        }
+        final String[] a = strng.split("\n");
+        String[] recs = null;
+        double sum = 0;
+
+        for (String str : a) {
+            if (str.startsWith(town + ":")) {
+                recs = str.split(":")[1].split(",");
+                break;
+            }
+        }
+        if (recs == null || recs.length == 0) {
+            return -1;
+        }
+
+        for (int i = 0; i < recs.length; i++) {
+
+            sum += Math.pow((Double.parseDouble(recs[i].split(" ")[1]) - mean(town, strng)), 2);
+        }
+        return sum/12.0;
     }
 
     @Override
