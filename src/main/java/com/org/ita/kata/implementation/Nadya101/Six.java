dce91152d372cee7b1cd38a6a888d4a5d27d61cb
Nadya Bryashchey <Angel.nadii36@gmail.com>
2021-10-28 12:05:56 +0300

    nadya101/#335 Ranking NBA teams

--- src/main/java/com/org/ita/kata/implementation/Nadya101/Six.java
+++ src/main/java/com/org/ita/kata/implementation/Nadya101/Six.java
@@ -54,7 +54,90 @@ public class Six implements com.org.ita.kata.Six {
 
     @Override
     public String nbaCup(String resultSheet, String toFind) {
-        return null;
+        int win = 0;
+        int draw = 0;
+        int lose = 0;
+        int scored = 0;
+        int conceded = 0;
+        int points = 0;
+        String[] parsingOneMatch;
+        boolean firstTeamParsed;
+
+        if (!resultSheet.contains(toFind)) {
+            return toFind + ":This team didn't play!";
+        }
+
+        String[] allGames = resultSheet.split(",");
+        for (String game : allGames) {
+            if (game.matches(".*[0-9]+[.][0-9]+.*")) {
+                return "Error(float number):" + game;
+            }
+        }
+
+        String[] firstTeam = new String[allGames.length];
+        int[] scoresFistTeam = new int[allGames.length];
+        String[] secondTeam = new String[allGames.length];
+        int[] scoresSecondTeam = new int[allGames.length];
+
+        for (int i = 0; i < allGames.length; i++) {
+            parsingOneMatch = allGames[i].split(" ");
+            firstTeam[i] = "";
+            secondTeam[i] = "";
+            firstTeamParsed = false;
+            for (int j = 0; j < parsingOneMatch.length; j++) {
+                if (!parsingOneMatch[j].matches("^[0-9]+$")) {
+                    if (firstTeamParsed) {
+                        secondTeam[i] += parsingOneMatch[j] + " ";
+                    } else {
+                        firstTeam[i] += parsingOneMatch[j] + " ";
+                    }
+                } else {
+                    if (!firstTeamParsed) {
+                        scoresFistTeam[i] = Integer.parseInt(parsingOneMatch[j]);
+                        firstTeamParsed = true;
+                    } else {
+                        scoresSecondTeam[i] = Integer.parseInt(parsingOneMatch[j]);
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < allGames.length; i++) {
+            if ((toFind + " ").equals(firstTeam[i])) {
+                if (scoresFistTeam[i] > scoresSecondTeam[i]) {
+                    win++;
+                    points += 3;
+                }
+                if (scoresFistTeam[i] == scoresSecondTeam[i]) {
+                    draw++;
+                    points += 1;
+                }
+                if (scoresFistTeam[i] < scoresSecondTeam[i]) {
+                    lose++;
+                }
+                scored += scoresFistTeam[i];
+                conceded += scoresSecondTeam[i];
+            }
+            if ((toFind + " ").equals(secondTeam[i])) {
+                if (scoresFistTeam[i] < scoresSecondTeam[i]) {
+                    win++;
+                    points += 3;
+                }
+                if (scoresFistTeam[i] == scoresSecondTeam[i]) {
+                    draw++;
+                    points += 1;
+                }
+                if (scoresFistTeam[i] > scoresSecondTeam[i]) {
+                    lose++;
+                }
+                scored += scoresSecondTeam[i];
+                conceded += scoresFistTeam[i];
+            }
+        }
+        if (scored == 0) {
+            return toFind + ":This team didn't play!";
+        } else {
+            return toFind + ":" + "W=" + win + ";D=" + draw + ";L=" + lose + ";Scored=" + scored + ";Conceded=" + conceded + ";Points=" + points;
+        }
     }
 
     @Override
