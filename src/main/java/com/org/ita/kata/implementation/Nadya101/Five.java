794bd669a2a16359f5ad4fb3b59857c4c5a82655
Nadya Bryashchey <Angel.nadii36@gmail.com>
2021-10-28 14:57:34 +0300

    nadya101/#343 Artificial Rain

--- src/main/java/com/org/ita/kata/implementation/Nadya101/Five.java
+++ src/main/java/com/org/ita/kata/implementation/Nadya101/Five.java
@@ -5,7 +5,49 @@ import java.math.BigInteger;
 public class Five implements com.org.ita.kata.Five {
     @Override
     public int artificialRain(int[] v) {
-        return 0;
+        int[] flowRight = Five.computeRightFlow(v);
+        int[] flowLeft = Five.computeLeftFlow(v);
+        int maxWateredPlains = 0;
+        for (int i = 0; i < flowLeft.length; i++) {
+            maxWateredPlains = Math.max(flowLeft[i] + flowRight[i] + 1, maxWateredPlains);
+        }
+        return maxWateredPlains;
+    }
+
+    static boolean canFlowLeft(int[] numbers, int i) {
+        if (i == 0)
+            return false;
+        return numbers[i - 1] <= numbers[i];
+    }
+
+    static boolean canFlowRight(int[] numbers, int i) {
+        if (numbers.length - 1 == i)
+            return false;
+        return numbers[i + 1] <= numbers[i];
+    }
+
+    static int[] computeLeftFlow(int[] numbers) {
+        int[] result = new int[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            if (canFlowLeft(numbers, i)) {
+                result[i] = result[i - 1] + 1;
+            } else {
+                result[i] = 0;
+            }
+        }
+        return result;
+    }
+
+    static int[] computeRightFlow(int[] numbers) {
+        int[] result = new int[numbers.length];
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            if (canFlowRight(numbers, i)) {
+                result[i] = result[i + 1] + 1;
+            } else {
+                result[i] = 0;
+            }
+        }
+        return result;
     }
 
     @Override
