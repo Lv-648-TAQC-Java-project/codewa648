0b3e2885c9bc3545541e246b07523697ea8c6110
Andriy Barskyi <barskyiandriy@gmail.com>
2021-10-28 19:33:54 +0300

    implement method artificialRain in Five

--- src/main/java/com/org/ita/kata/implementation/AndriyBarskyi/Five.java
+++ src/main/java/com/org/ita/kata/implementation/AndriyBarskyi/Five.java
@@ -5,7 +5,21 @@ import java.math.BigInteger;
 public class Five implements com.org.ita.kata.Five {
     @Override
     public int artificialRain(int[] v) {
-        return 0;
+        int height = 0;
+        int size = 1;
+        int low = 0;
+        for (int i = 1; i < v.length; i++) {
+            int k = v[i];
+            if (k > v[i - 1]) {
+                height = Math.max(height, size);
+                size = i - low;
+            }
+            if (k < v[i - 1]) {
+                low = i;
+            }
+            size++;
+        }
+        return Math.max(height, size);
     }
 
     @Override
