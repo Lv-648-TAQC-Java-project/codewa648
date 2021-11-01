c9c50938e64590e8f41692cdeda90cfcbc3219ec
Nadya Bryashchey <Angel.nadii36@gmail.com>
2021-11-01 10:50:27 +0200

    nadya101/#354 Find the smallest

--- src/main/java/com/org/ita/kata/implementation/Nadya101/Five.java
+++ src/main/java/com/org/ita/kata/implementation/Nadya101/Five.java
@@ -30,6 +30,15 @@ public class Five implements com.org.ita.kata.Five {
 
     @Override
     public long[] smallest(long n) {
-        return new long[0];
+        String str = ""+n;
+        long[] result = new long[]{Long.MAX_VALUE,0,0};
+        for (int i=str.length()-1; i>=0; i--) {
+            String str1=str.substring(0,i)+str.substring(i+1);
+            for (int j=str.length()-1; j>=0; j--) {
+                long tmp = Long.parseLong(str1.substring(0,j)+str.charAt(i)+str1.substring(j));
+                if (tmp <= result[0]) result = new long[]{tmp,i,j};
+            }
+        }
+        return result;
     }
 }
