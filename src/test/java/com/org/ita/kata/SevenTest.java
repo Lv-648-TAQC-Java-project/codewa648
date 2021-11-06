e89db635af53df58c4a4fa4d327ed0b6105bb0cb
Nadya Bryashchey <Angel.nadii36@gmail.com>
2021-11-06 15:17:13 +0200

    nadya101/#415 testNG for newAvg method

--- src/test/java/com/org/ita/kata/SevenTest.java
+++ src/test/java/com/org/ita/kata/SevenTest.java
@@ -1,21 +1,34 @@
 package com.org.ita.kata;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import org.testng.Assert;
 
-public class SevenTest {
+public class SevenTest extends DataProviderUserImplementation {
 
-  @Test
-  public void newAvgTest() {
-    throw new RuntimeException("Test not implemented");
-  }
+    @DataProvider
+    public Object[][] newAvgValidData() {
+        Object[][] param = new Object[][]{
+                {new double[]{14.0, 30.0, 5.0, 7.0, 9.0, 11.0, 16.0}, 90, 628},
+                {new double[]{14, 30, 5, 7, 9, 11, 15}, 92, 645},
+                {new double[]{115, 8, 9, 12, 17, 88, 4}, 77, 363},
+                {new double[]{12,88,100,90,316,5}, 99, 82}
+        };
+        return combine(implementationsSevenKataDataProvider(), param);
+    }
 
-  @Test
-  public void seriesSumTest() {
-    throw new RuntimeException("Test not implemented");
-  }
+    @Test(dataProvider = "newAvgValidData")
+    public void newAvgTest(Seven seven, double[] array, double navg, long expected) {
+        Assert.assertEquals(seven.newAvg(array, navg), expected);
+    }
 
-  @Test
-  public void whereIsHeTest() {
-    throw new RuntimeException("Test not implemented");
-  }
+    @Test
+    public void seriesSumTest() {
+        throw new RuntimeException("Test not implemented");
+    }
+
+    @Test
+    public void whereIsHeTest() {
+        throw new RuntimeException("Test not implemented");
+    }
 }
