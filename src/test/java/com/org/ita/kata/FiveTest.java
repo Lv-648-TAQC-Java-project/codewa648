22f7c02b8792e3de10215abf816c2e663d276ab4
MakKeywa <mak.keywa@gmail.com>
2021-11-06 14:53:08 +0200

    added test for Gap in Primes

--- src/test/java/com/org/ita/kata/FiveTest.java
+++ src/test/java/com/org/ita/kata/FiveTest.java
@@ -1,17 +1,32 @@
 package com.org.ita.kata;
 
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-public class FiveTest {
+public class FiveTest extends DataProviderUserImplementation{
 
   @Test
   public void artificialRainTest() {
     throw new RuntimeException("Test not implemented");
   }
 
-  @Test
-  public void gapTest() {
-    throw new RuntimeException("Test not implemented");
+  @DataProvider
+  public Object[][] gapDataProvider() {
+    Object[][] param = new Object[][]{
+            {2, 100, 110, new long[]{101, 103}},
+            {4, 100, 110, new long[]{103, 107}},
+            {6, 100, 110, null},
+            {8, 300, 400, new long[]{359, 367}},
+            {10, 300, 400, new long[]{337, 347}},
+
+    };
+    return combine(implementationsFiveKataDataProvider(), param);
+  }
+
+  @Test(dataProvider = "gapDataProvider")
+  public void gapTest(Five five, int gap, long start, long end, long[] expected) {
+    Assert.assertEquals(five.gap(gap, start, end), expected);
   }
 
   @Test
