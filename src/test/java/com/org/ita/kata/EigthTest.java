793b75f69edad9d78829606db6d950a69b4ed028
MakKeywa <mak.keywa@gmail.com>
2021-11-06 14:49:47 +0200

    added test for squareOrSquareRoot

--- src/test/java/com/org/ita/kata/EigthTest.java
+++ src/test/java/com/org/ita/kata/EigthTest.java
@@ -6,6 +6,8 @@ import org.testng.annotations.DataProvider;
 
 import org.testng.Assert;
 
+import java.util.Arrays;
+
 public class EigthTest extends DataProviderUserImplementation {
 
     @DataProvider
@@ -57,9 +59,22 @@ public class EigthTest extends DataProviderUserImplementation {
 	throw new RuntimeException("Test not implemented");
     }
 
-    @Test
-    public void squareOrSquareRootTest() {
-	throw new RuntimeException("Test not implemented");
+    @DataProvider
+    public Object[][] squareOrSquareRootDataProvider() {
+        Object[][] param = new Object[][]{
+                {new int[]{4, 3, 9, 7, 2, 1}, new int[]{2, 9, 3, 49, 4, 1}}
+//                ,
+//                {new int[]{100, 101, 5, 5, 1, 1}, new int[]{10, 10201, 25, 25, 1, 1}}
+//                ,
+//                {new int[]{1, 2, 3, 4, 5, 6}, new int[]{1, 4, 9, 2, 25, 36}}
+        };
+        return combine(implementationsEightKataDataProvider(), param);
+    }
+
+    @Test(dataProvider = "squareOrSquareRootDataProvider")
+    public void squareOrSquareRootTest(Eight eight, int[] input, int[] expected) {
+        int[] result = eight.squareOrSquareRoot(input);
+        Assert.assertEquals(result, expected);
     }
 
     @Test
