bfe093470f5446514c225ef0995f1a45cf7e4148
Andriy Barskyi <barskyiandriy@gmail.com>
2021-11-06 11:05:55 +0200

    refactor Eigth

--- src/main/java/com/org/ita/kata/implementation/AndriyBarskyi/Eigth.java
+++ src/main/java/com/org/ita/kata/implementation/AndriyBarskyi/Eigth.java
@@ -13,23 +13,26 @@ public class Eigth implements Eight {
 
     @Override
     public double getVolumeOfCuboid(double length, double width, double height) {
-        if (length > 0 && height > 0 && width > 0)
+        if (length > 0 && height > 0 && width > 0) {
             return length * height * width;
+        }
         return 0;
     }
 
     @Override
     public float mpgToKPM(float mpg) {
-        return Math.round(mpg * 35.4006004) / 100f;
+        if (mpg > 0) {
+            return Math.round(mpg * 35.4006004) / 100f;
+        }
+        return 0;
     }
 
     @Override
     public int[] squareOrSquareRoot(int[] array) {
         for (int i = 0; i < array.length; i++) {
-            if (Math.sqrt(array[i]) == (int)Math.sqrt(array[i])) {
-                array[i] = (int)Math.sqrt(array[i]);
-            }
-            else {
+            if (Math.sqrt(array[i]) == (int) Math.sqrt(array[i])) {
+                array[i] = (int) Math.sqrt(array[i]);
+            } else {
                 array[i] *= array[i];
             }
         }
@@ -40,12 +43,12 @@ public class Eigth implements Eight {
     public int[] countPositivesSumNegatives(int[] input) {
         int[] result = new int[2];
         if (input == null || input.length == 0) {
-            return new int[] {};
+            return new int[]{};
         }
-        for (int i = 0; i < input.length; i++) {
-            if (input[i] < 0) {
-                result[1] += input[i];
-            } else if (input[i] > 0) {
+        for (int j : input) {
+            if (j < 0) {
+                result[1] += j;
+            } else if (j > 0) {
                 result[0]++;
             }
         }
@@ -70,7 +73,7 @@ public class Eigth implements Eight {
         for (int i = 1; i <= n - 1; i++) {
             fact = fact * i;
         }
-        return ((fact + 1) / (n * n)) == (int)((fact + 1) / (n * n));
+        return ((fact + 1) / (n * n)) == (int) ((fact + 1) / (n * n));
     }
 
     @Override
@@ -84,16 +87,16 @@ public class Eigth implements Eight {
             return new int[]{};
         }
         int count = 0;
-        for(int i = 0; i < numbers.length; i++) {
-            if (numbers[i] % divider == 0) {
+        for (int number : numbers) {
+            if (number % divider == 0) {
                 count++;
             }
         }
-        int divisible[] = new int[count];
+        int[] divisible = new int[count];
         int j = 0;
-        for(int i: numbers) {
-            if (i % divider == 0) {
-                divisible[j] = i;
+        for (int num : numbers) {
+            if (num % divider == 0) {
+                divisible[j] = num;
                 j++;
             }
         }
