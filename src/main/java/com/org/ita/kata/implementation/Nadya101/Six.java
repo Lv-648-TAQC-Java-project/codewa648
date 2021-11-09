51964d10dff45f43bb9d0c6844410070ca28678f
Nadya Bryashchey <Angel.nadii36@gmail.com>
2021-11-09 02:10:44 +0200

    nadya101/#335 nbaCup method modified

--- src/main/java/com/org/ita/kata/implementation/Nadya101/Six.java
+++ src/main/java/com/org/ita/kata/implementation/Nadya101/Six.java
@@ -63,6 +63,10 @@ public class Six implements com.org.ita.kata.Six {
         String[] parsingOneMatch;
         boolean firstTeamParsed;
 
+        if(toFind.isEmpty()){
+            return "";
+        }
+
         if (!resultSheet.contains(toFind)) {
             return toFind + ":This team didn't play!";
         }
