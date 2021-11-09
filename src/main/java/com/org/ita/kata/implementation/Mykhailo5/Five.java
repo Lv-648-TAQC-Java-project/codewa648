59a6e85969e20055aa6a78dbd6f7874544639548
Mykhailo5 <mykhailomykhailyshyn@gmail.com>
2021-11-09 19:17:01 +0200

    fixed perimeter task

--- src/main/java/com/org/ita/kata/implementation/Mykhailo5/Five.java
+++ src/main/java/com/org/ita/kata/implementation/Mykhailo5/Five.java
@@ -55,6 +55,9 @@ public class Five implements com.org.ita.kata.Five {
 
     @Override
     public BigInteger perimeter(BigInteger n) {
+        if(n.compareTo(BigInteger.valueOf(2))<0){
+            return BigInteger.ZERO;
+        }
         BigInteger p0 = BigInteger.valueOf(0);
         BigInteger p1 = BigInteger.valueOf(1);
         BigInteger p2 = BigInteger.valueOf(1);
