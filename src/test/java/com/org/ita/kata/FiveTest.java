6ca5046a17b5e023fa88e7091128825871a9f340
MakKeywa <mak.keywa@gmail.com>
2021-11-09 18:40:09 +0200

    fix test gap

--- src/test/java/com/org/ita/kata/FiveTest.java
+++ src/test/java/com/org/ita/kata/FiveTest.java
@@ -31,7 +31,6 @@ public class FiveTest extends DataProviderUserImplementation {
         Object[][] param = new Object[][]{
                 {2, 100, 110, new long[]{101, 103}},
                 {4, 100, 110, new long[]{103, 107}},
-                {6, 100, 110, null},
                 {8, 300, 400, new long[]{359, 367}},
                 {10, 300, 400, new long[]{337, 347}},
 
